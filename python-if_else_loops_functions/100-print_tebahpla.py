bb0fd5c8c6e595cb90e7325f7c4323eb449093e3
Fuad-hack <fuadkhanaliyev@gmail.com>
2025-06-15 16:25:45 +0000

    iedncd

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-print("".join([chr(c) if i % 2 == 0 else chr(c - 32) for i, c in enumerate(range(122, 96, -1))]), end="")
+# 100-print_tebahpla.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
