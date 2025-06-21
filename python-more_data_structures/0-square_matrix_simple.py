1666f8970988cd7bb1a314d1ee27b1fc1d153f2b
root <fuad@gmail.com>
2025-06-21 19:14:22 +0000

    task-0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
-"""
-    /** square_matrix_simple - computes the square value of integers in matrix
-      * @matrix: 2 dimensional matrix
-      *
-      * Return: matrix
-      */
-"""
-
-
-def square_matrix_simple(matrix=[]):
-    return list(map(lambda m: list(map(lambda y: y**2, m)), matrix))
-
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
