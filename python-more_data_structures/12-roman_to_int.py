6e666ce6e4e41c1de4934c7e1278808ef732cad8
root <root@ip-10-42-159-19.ec2.internal>
2025-06-24 13:34:18 +0000

    wfncsi

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
-"""
-    /**
-      * roman_to_int - converts a string roman numerals to integer
-      * @roman_string: string
-      *
-      * Return: integer
-      */
-"""
-
-
 def roman_to_int(roman_string):
     result = 0
-    if type(roman_string) == str and roman_string is not None:
-        string = roman_string
-        strlen = len(string)
+    if roman_string is not None and isinstance(roman_string, str):
         std = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        strlen = len(roman_string)
         for i in range(strlen):
-            if (i > 0) and (std[string[i]] > std[string[i - 1]]):
-                result += int(std[string[i]]) - int(std[string[i - 1]]) * 2
+            if i > 0 and std[roman_string[i]] > std[roman_string[i - 1]]:
+                result += std[roman_string[i]] - 2 * std[roman_string[i - 1]]
             else:
-                result += std[string[i]]
+                result += std[roman_string[i]]
     return result
+
