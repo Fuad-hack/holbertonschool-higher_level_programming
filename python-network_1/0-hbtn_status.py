694136f4378fd0e046fc9b84a5ebe52bc0020e7d
root <you@example.com>
2025-07-22 06:56:54 +0000

    Task - 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-Task - 0 
+This script fetches https://intranet.hbtn.io/status using urllib
+and displays information about the response body.
 """
 
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode('utf-8')))
+        body = response.read()
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body.decode('utf-8')))
