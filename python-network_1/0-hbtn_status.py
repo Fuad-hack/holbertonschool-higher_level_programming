85a9634c9cdf2bb7c2821f780697a23c2e6f5233
root <you@example.com>
2025-07-22 06:34:05 +0000

    Task - 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-# -*- config: utf-8 -*-
-
-"""
-Task - 0
-"""
+"""Task - 0"""
 
 import urllib.request
+
 url = 'https://intranet.hbtn.io/status'
 headers = {'User-Agent': 'Mozilla/5.0'}
+
 req = urllib.request.Request(url, headers=headers)
 
-with urllib.request.urlopen(req777) as response:
-	body = response.read()
+with urllib.request.urlopen(req) as response:
+    body = response.read()
 
-	print("Body response:")
-	print("\t- type: {}".format(type(body)))
-	print("\t- content: {}".format(body))
-	print("\t- utf8 content: {}".format(body.decode('utf-8')))
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
+
