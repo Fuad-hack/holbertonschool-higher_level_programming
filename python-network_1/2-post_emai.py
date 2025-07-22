ce4ae5d467a02b789da68f46ef1ed52095b14b37
root <you@example.com>
2025-07-22 08:50:35 +0000

    Task - 2

---
+++ python-network_1/2-post_emai.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response (decoded in utf-8).
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    # Prepare the data to be sent in POST request
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+    
+    # Create the request with POST data
+    request = urllib.request.Request(url, data=data)
+    
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
